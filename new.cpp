a356f06723d3a5ef6405f87c6b7e186a1496855e
HAPPIOcrz007 <197754596+HAPPIOcrz007@users.noreply.github.com>
2026-07-11 01:20:01 +0530

    fixed my enum class Stage

--- new.cpp
+++ new.cpp
@@ -16,15 +16,41 @@ uint64_t generate_order_ID(const std::string &ticker);
     to int
 */
 enum class Stage{
-    Added_in_Book,
-    Matching,
+    New,
+    Partially_filled,
     Filled,
-    Recorded,
-
-    Modified,
-    Re_Added_in_Book,
-
     Cancelled,
-    Flagged
+    Replaced
+    // ? what do real systems use
+    /* ANS -
+        The core values are:
+        New, Partially Filled, Filled, Cancelled, Replaced,
+        Pending Cancel, Pending New, Pending Replace,
+        Rejected, Expired, Suspended, Done for Day
+    */
+    // ? why not using the pending** cases
+    /* ANS -
+        I looked at FIX OrdStatus,
+        took the subset that applies to a synchronous
+        single-threaded engine, and left the Pending states
+        as a known gap that matters once I go async
+    */
+   // ? not using stage - replaced here
+   /* ANS - 
+        many systems allow replacing of order specifications like
+        price and volume
+        so when a change is made in most systems, mainly price change
+        the system cancels the current order and gives a new orderId making
+        a new order at the timestamp of when the replacement is accepted by the
+        system, this is also same in volume increase as its like a boost someone
+        would get increaseing the position at the last timestamp
+        but if a entity is shrinking its volume we can allow them to keep the
+        same Id and the last timestamp as it wont impact others
+        (According to process of CME Group)
+        changing sides gives different orderID too at new timestamp
+
+        so we keep replaced_orderId tag blank for all orders and if they are
+        a continuation of another order we put the last order's id there
+   */
 };
 
