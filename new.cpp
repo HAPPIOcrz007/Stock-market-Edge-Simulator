326d700d45470b1d3d99c61d983e347da21fe2e4
HAPPIOcrz007 <197754596+HAPPIOcrz007@users.noreply.github.com>
2026-07-10 13:24:19 +0530

    started restructuring

--- new.cpp
+++ new.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 #include <chrono>
+
+uint64_t generate_order_ID(const std::string &ticker);
+
+// ? - why enum class
+/* ANS -
+    enum - it is better in understanding than
+    integers to the reader*/
+// ? but ... why enum class
+/* ANS -
+    enums hold integer value at the back so classing
+    reduces that leak into the global scope 
+    protecting and reducing errors
+    classed enums cannot be implicitly converted
+    to int
+*/
+enum class Stage{
+    Added_in_Book,
+    Matching,
+    Filled,
+    Recorded,
+
+    Modified,
+    Re_Added_in_Book,
+
+    Cancelled,
+    Flagged
+};
+
