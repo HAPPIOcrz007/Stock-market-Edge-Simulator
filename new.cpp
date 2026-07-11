652629d4f5f9a72461a8c168db435f9d2d65d7ac
HAPPIOcrz007 <197754596+HAPPIOcrz007@users.noreply.github.com>
2026-07-11 23:55:00 +0530

    update int the restructuring Struct Order

--- new.cpp
+++ new.cpp
@@ -10,12 +10,13 @@ uint64_t generate_order_ID(const std::string &ticker);
 // ? but ... why enum class
 /* ANS -
     enums hold integer value at the back so classing
-    reduces that leak into the global scope 
+    reduces that leak into the global scope
     protecting and reducing errors
     classed enums cannot be implicitly converted
     to int
 */
-enum class Stage{
+enum class Stage
+{
     New,
     Partially_filled,
     Filled,
@@ -35,22 +36,25 @@ enum class Stage{
         single-threaded engine, and left the Pending states
         as a known gap that matters once I go async
     */
-   // ? not using stage - replaced here
-   /* ANS - 
-        many systems allow replacing of order specifications like
-        price and volume
-        so when a change is made in most systems, mainly price change
-        the system cancels the current order and gives a new orderId making
-        a new order at the timestamp of when the replacement is accepted by the
-        system, this is also same in volume increase as its like a boost someone
-        would get increaseing the position at the last timestamp
-        but if a entity is shrinking its volume we can allow them to keep the
-        same Id and the last timestamp as it wont impact others
-        (According to process of CME Group)
-        changing sides gives different orderID too at new timestamp
+    // ? not using stage - replaced here
+    /* ANS -
+         many systems allow replacing of order specifications like
+         price and volume
+         so when a change is made in most systems, mainly price change
+         the system cancels the current order and gives a new orderId making
+         a new order at the timestamp of when the replacement is accepted by the
+         system, this is also same in volume increase as its like a boost someone
+         would get increaseing the position at the last timestamp
+         but if a entity is shrinking its volume we can allow them to keep the
+         same Id and the last timestamp as it wont impact others
+         (According to process of CME Group)
+         changing sides gives different orderID too at new timestamp
 
-        so we keep replaced_orderId tag blank for all orders and if they are
-        a continuation of another order we put the last order's id there
-   */
+         so we keep replaced_orderId tag blank for all orders and if they are
+         a continuation of another order we put the last order's id there
+    */
 };
 
+struct Order
+{
+};
